a54c9eed05451a88d887b3f8a2f9c325e4347c13
grignetta <121875111+grignetta@users.noreply.github.com>
2025-09-11 23:27:45 +0200

    port is restricted to 1-65535, password to 64 chars

---
+++ srcs/utils/Controls.cpp
@@ -0,0 +1,33 @@
+#include "Server.hpp"
+
+bool controlPort(char* argv)
+{   
+	std::string portStr = argv;
+	if (portStr.length() > 5)
+	{
+		std::cerr << "Invalid port: must be 1–65535\n";
+		return false;
+	}   
+	int port = std::atoi(portStr.c_str());
+	if (port < 1 || port > 65535)
+	{
+		std::cerr << "Invalid port: must be 1–65535\n";
+		return false;
+	}
+	return true;
+}
+
+bool controlPassword(std::string& password)
+{
+    if (password.empty() || password.length() > 64)
+        return false;
+
+    for (size_t i = 0; i < password.size(); ++i)
+	{
+        if (std::isspace(static_cast<unsigned char>(password[i])))
+		{
+            return false;
+        }
+    }
+    return true;
+}
